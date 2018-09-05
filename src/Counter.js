126ee93774e7623bcd8fc4147d3126cd15fc318a
fyz <fyz97634@163.com>
2018-09-05 20:41:17 +0800

    redux

---
+++ src/Counter.js
@@ -0,0 +1,57 @@
+/**
+ * Created by apple on 18/9/5.
+ */
+import React,{Component} from 'react';
+import ReactDOM from 'react-dom';
+import {createStore} from './redux'
+import 'bootstrap/dist/css/bootstrap.css';
+
+
+
+const INCREASE = 'INCREASE';
+const DECREASE = 'DECREASE';
+
+export default class Counter extends Component {
+
+    render(){
+
+        return (
+            <div>
+                <p>{store.getState().num}</p>
+                <button className="btn btn-primary" onClick={()=> {store.dispatch({type:INCREASE,amount:3})}}>+</button>
+                <button className="btn btn-primary" onClick={()=> {store.dispatch({type:DECREASE,amount:2})}}>-</button>
+
+            </div>
+        )
+    }
+}
+
+let reducer = (state={num:0},action)=>{
+
+    if(action === undefined) return state;
+
+    switch (action.type){
+
+        case INCREASE:
+            return {num:state.num + action.amount}
+
+        case DECREASE:
+            return {num:state.num - action.amount}
+
+        default:
+             return state;
+    }
+}
+
+
+//创建一个仓库
+let store = createStore(reducer);
+
+let render = ()=>{
+    ReactDOM.render(<Counter/>,document.querySelector('#root'));
+}
+
+render();
+
+//订阅
+store.subscribe(render);
---
+++ src/redux.js
@@ -0,0 +1,50 @@
+/**
+ * Created by apple on 18/9/5.
+ */
+
+const createStore = (reducer)=>{
+
+    // 状态
+    let state;
+
+    //监听者对象数组
+    let listeners = [];
+
+    //获取状态值
+    let getState = ()=> state;
+
+    //发射任务
+    let dispatch = (action)=> {
+
+        state = reducer(state,action);
+
+        listeners.forEach(listener => listener());
+    }
+
+    //执行发射任务
+    dispatch();
+
+
+    //订阅
+    let subscribe = (listener)=>{
+
+        //向监听者对象数组添加监听者
+        listeners.push(listener);
+
+        //返回一个 取消订阅的函数
+        return () => {
+
+            listeners.filter(l => listener !== l);
+        }
+    }
+
+
+    return {
+        getState,
+        dispatch,
+        subscribe
+    }
+
+}
+
+export {createStore}
